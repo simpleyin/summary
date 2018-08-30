3cadb27defe5ecb6bffe0f575908ce9f6318a058
黄 <huang@Simpleyin.local>
2018-08-30 11:08:15 +0800

    update

--- playground.js
+++ playground.js
@@ -1,18 +1,32 @@
-// //快排
-// var mock = [11,0,1,3,8,33];
-// var quikcSort = function(arr) {
-//     //1. 找到基础数
+//快排
+var mock = [6, 1, 2, 7, 9, 3, 4, 5, 10, 8];
+var quickSort = function (arr) {
+    var base = arr[0];
+    var i = 0, j = arr.length - 1;
+    if (arr.length === 1) return arr;
+    while (i < j) {
+        if (arr[j] >= base) {
+            j--;
+        } else if (arr[i] <= base) {
+            i++;
+        } else {
+            swap(i, j, arr);
+            j = j - 1;
+            i = i + 1;
+        }
+    }
+    swap(0, i, arr);
+    var left = quickSort(arr.slice(0, i));
+    var right = quickSort(arr.slice(i + 1, arr.length));
 
-// }
-
-// //sort的实现原理
-
-if (true) {
-    var a = "a";
-    let b = "b";
-    const c = "c";
+    
+    return [...left, base, ...right];
 }
 
-console.log(a);
-//console.log(b);
-console.log(c);
+var swap = function (i, j, arr) {
+    var t = arr[j];
+    arr[j] = arr[i];
+    arr[i] = t;
+}
+
+quickSort(mock);
